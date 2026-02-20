b7f58a2b418a72691785e630d9d5a4a34dcf20f1
Jasmine <jasmineliu331@gmail.com>
2026-02-19 17:17:13 -0800

    little guy

--- app/cat/page.tsx
+++ app/cat/page.tsx
@@ -1,5 +1,117 @@
+'use client'
+import { useState, useEffect } from "react";
+
+const frames = [`
+\n
+           _____  \n
+    　 　 ／ >    フ \n
+　　　 　　| 　 _　 _| \n
+　 　 　　／  ミ _xノ\n
+　　 　  /　　　 　 |\n
+　　　  /　 ヽ　　 ﾉ\n
+　 ／￣|　　 |　|　|\n
+　| (￣ヽ＿_ヽ_)_)\n
+　 ＼二二つ\n
+`,
+`
+\n
+           _____  \n
+    　 　 ／ >    フ \n
+　　　 　　| 　 _　 _| z\n
+　 　 　　／  ミ _xノ\n
+　　 　  /　　　 　 |\n
+　　　  /　 ヽ　　 ﾉ\n
+　 ／￣|　　 |　|　|\n
+　| (￣ヽ＿_ヽ_)_)\n
+　 ＼二二つ\n
+`,
+`
+\n
+           _____  \n
+    　 　 ／ >    フ  Z\n
+　　　 　　| 　 _　 _| z\n
+　 　 　　／  ミ _xノ\n
+　　 　  /　　　 　 |\n
+　　　  /　 ヽ　　 ﾉ\n
+　 ／￣|　　 |　|　|\n
+　| (￣ヽ＿_ヽ_)_)\n
+　 ＼二二つ\n
+`,
+`
+                    \n
+           _____      z\n
+    　 　 ／ >    フ  Z\n
+　　　 　　| 　 _　 _| z\n
+　 　 　　／  ミ _xノ\n
+　　 　  /　　　 　 |\n
+　　　  /　 ヽ　　 ﾉ\n
+　 ／￣|　　 |　|　|\n
+　| (￣ヽ＿_ヽ_)_)\n
+　 ＼二二つ\n
+`,
+`
+                   z \n
+           _____      z\n
+    　 　 ／ >    フ  Z\n
+　　　 　　| 　 _　 _| z\n
+　 　 　　／  ミ _xノ\n
+　　 　  /　　　 　 |\n
+　　　  /　 ヽ　　 ﾉ\n
+　 ／￣|　　 |　|　|\n
+　| (￣ヽ＿_ヽ_)_)\n
+　 ＼二二つ\n
+`,
+`
+\n
+           _____  \n
+    　 　 ／ >    フ \n
+　　　 　　| 　 o　 o| \n
+　 　 　　／  ミ _xノ\n
+　　 　  /　　　 　 |\n
+　　　  /　 ヽ　　 ﾉ\n
+　 ／￣|　　 |　|　|\n
+　| (￣ヽ＿_ヽ_)_)\n
+　 ＼二二つ\n
+`,
+];
+
 export default function CatPage() {
+  const [frame, setFrame] = useState(0);
+  const [awake, setAwake] = useState(false);
+
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout
+    if (!awake) {
+      intervalId = setInterval(() => {
+        setFrame(prev => {
+          if (prev === 4) {
+            return 0
+          } else {
+            return prev + 1
+          }
+        })
+      }, 1000);
+    } else {
+      setFrame(5)
+    }
+    
+    return () => {
+      setFrame(0)
+      if (intervalId) clearInterval(intervalId);
+    }
+  }, [awake])
+
   return (
-    <h1>hi</h1>
+    <pre 
+      onMouseEnter={() => {
+        setAwake(true)
+      }}
+      onMouseLeave={() => {
+        setAwake(false)
+      }}
+      style={{ fontFamily: "monospace", lineHeight: "0.7" }}>
+      {frames[frame]}
+    </pre>
+
   )
 }
